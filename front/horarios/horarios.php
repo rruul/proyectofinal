5285a3272846224e09de42c29f0f79a5952852d3
gonzalezjennifer <62447915+gonzalezjennifer@users.noreply.github.com>
2023-06-10 23:42:17 -0600

    yooooooooooo

--- front/horarios/horarios.php
+++ front/horarios/horarios.php
@@ -6,20 +6,20 @@
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css">
   <link rel="stylesheet" href="./style/style.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <title>Título de tu página</title>
+  <title>Horarios</title>
 </head>
-<body>
+<body style="background-color: #F1F6F9!important;">
   <?php include './headerhora.html';?>
   <main>
     <div class="container-fluid">
       <div class="row">
         <div class="col">
-          <h2 class="text-center mb-3" id="titulo"></h2>
+          <h2 class="text-center mb-3" id="titulo" style="margin-top: 10px;"></h2>
         </div>
       </div>
       <div class="container">
         <div class="ratio" style="--bs-aspect-ratio: 39%;">
-          <iframe src="" style="width:100%;" id="pdf" frameborder="0" ></iframe>
+          <iframe src="" style="width:100%; height:80vh" id="pdf" frameborder="0" ></iframe>
         </div>
       </div>
     </div>
